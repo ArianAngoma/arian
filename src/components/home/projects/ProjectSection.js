e0c6db67be6a312b5ab5c7a35d604c92846b032b
unknown <arian.angoma@tecsup.edu.pe>
2021-10-11 02:33:50 -0500

    ProjectItem agregado

---
+++ src/components/home/projects/ProjectSection.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import SwiperCore, {Navigation} from 'swiper';
+import 'swiper/swiper-bundle.min.css';
+
+export const ProjectSection = () => {
+
+}
