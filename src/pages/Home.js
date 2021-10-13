00a6caad9f019ae5e9cdb431eb9315cf2046af04
unknown <arian.angoma@tecsup.edu.pe>
2021-10-12 22:26:37 -0500

    Sección de Testimonios

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import {HeroSection} from '../components/home/hero/HeroSection';
 import {AboutSection} from '../components/home/about/AboutSection';
 import {ServicesSection} from '../components/home/services/ServicesSection';
 import {ProjectSection} from '../components/home/projects/ProjectSection';
+import {TestimonialsSection} from '../components/home/testimonials/TestimonialsSection';
 
 export const Home = () => {
     return (
@@ -11,6 +12,7 @@ export const Home = () => {
             <AboutSection/>
             <ServicesSection/>
             <ProjectSection/>
+            <TestimonialsSection/>
         </div>
     )
 }
