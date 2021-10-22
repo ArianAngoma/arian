d55bada7e1a0ac670dec55171151719faf6015d4
unknown <arian.angoma@tecsup.edu.pe>
2021-10-22 03:01:12 -0500

    Proyecto agregado y CV actualizado

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -3,14 +3,16 @@ import {v4 as uuidv4} from 'uuid';
 /* calendar-app */
 import CalendarAppImg from '../images/projects/calendar-app.jpeg';
 /* portfolio arianjs */
-import PortfioArianJs from '../images/projects/portfolio-arianjs.jpeg';
+import PortfolioArianJs from '../images/projects/portfolio-arianjs.jpeg';
+/* task-app */
+import TaskAppImg from '../images/projects/task-app.jpeg';
 
 export const projects = [
     {
         id: uuidv4(),
         name: 'Portfolio ArianJs',
         desc: 'Mi portafolio, donde estaré subiendo los proyectos en los que colaboro y/o realizo. Desarrollada en ReactJs y NodeJs.',
-        img: PortfioArianJs,
+        img: PortfolioArianJs,
         link: 'https://arianjs.herokuapp.com/',
         repoFrontend: 'https://github.com/ArianAngoma/arian',
         repoBackend: 'https://github.com/ArianAngoma/arian-api'
@@ -26,11 +28,11 @@ export const projects = [
     },
     {
         id: uuidv4(),
-        name: 'Calendar App',
-        desc: 'Una aplicación web para agendar notas en un calendario por usuario, Desarrollada en ReactJs y NodeJs.',
-        img: CalendarAppImg,
-        link: 'https://calendar-app-arianjs.herokuapp.com/',
-        repoFrontend: 'https://github.com/ArianAngoma/calendar-app-frontend-react',
-        repoBackend: 'https://github.com/ArianAngoma/calendar-app-backend-node'
+        name: 'Task App',
+        desc: 'Una aplicación web para gestionar proyectos y tareas, Desarrollada en ReactJs y NodeJs.',
+        img: TaskAppImg,
+        link: 'https://task-app-arianjs.netlify.app/',
+        repoFrontend: 'https://github.com/ArianAngoma/task-app-frontend-react',
+        repoBackend: 'https://github.com/ArianAngoma/task-app-backend-node'
     }
 ]
